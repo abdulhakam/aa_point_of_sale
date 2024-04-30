34854d0b93947009f62e1b9fb1afcc443d292b5e
Abdul Hakam <zehas.11@gmail.com>
2024-04-30 19:58:03 +0500

    added sales ledger by booker

---
+++ backend/pb_migrations/1714487261_updated_ledger_booker.js
@@ -0,0 +1,318 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("vpuhx1oo254tbm8")
+
+  collection.options = {
+    "query": "SELECT \n  p1.id,\n  p1.description,\n  p1.created,\n  p1.updated,\n  p1.payment_date AS dated,\n  p1.invoice,\n  p1.party,-- parties.type,\n  p1.type,\n  p1.paid,\n  (CASE WHEN p1.type = 'recieving' THEN \n      (CASE WHEN p1.paid = TRUE THEN p1.amount\n       ELSE 0 \n       END) \n   ELSE (CASE WHEN p1.type = 'sending' THEN \n           (CASE WHEN p1.paid = FALSE THEN p1.amount\n            ELSE 0 \n            END) \n         ELSE 0 -- return case missing\n         END)\n   END) AS credit,\n  (CASE WHEN p1.type = 'sending' THEN \n      (CASE WHEN p1.paid = TRUE THEN p1.amount\n       ELSE 0 \n       END) \n   ELSE (CASE WHEN p1.type = 'recieving' THEN \n           (CASE WHEN p1.paid = FALSE THEN p1.amount\n            ELSE 0 \n            END) \n         ELSE 0 -- return case missing\n         END)\n   END) AS debit\nFROM payments_view p1 -- JOIN parties ON p1.party = parties.id\n  ORDER BY p1.payment_date\n"
+  }
+
+  // remove
+  collection.schema.removeField("x1kfc8ab")
+
+  // remove
+  collection.schema.removeField("gkyyje5k")
+
+  // remove
+  collection.schema.removeField("12dpfahq")
+
+  // remove
+  collection.schema.removeField("85rztpen")
+
+  // remove
+  collection.schema.removeField("jw0gbypl")
+
+  // remove
+  collection.schema.removeField("vfao2udd")
+
+  // remove
+  collection.schema.removeField("dpkto1ef")
+
+  // remove
+  collection.schema.removeField("chj5hhku")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "lehnhtov",
+    "name": "description",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ddr5l4yz",
+    "name": "dated",
+    "type": "json",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 1
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "menar0a5",
+    "name": "invoice",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "0pwj3vph7ag7nc2",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "kda64c2c",
+    "name": "party",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "qhdujcbptsh2v29",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "heqmbep8",
+    "name": "type",
+    "type": "select",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "values": [
+        "sending",
+        "recieving",
+        "return"
+      ]
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "fn7simnp",
+    "name": "paid",
+    "type": "bool",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "8ev2sk5b",
+    "name": "credit",
+    "type": "json",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 1
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "dgmmqzpf",
+    "name": "debit",
+    "type": "json",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 1
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("vpuhx1oo254tbm8")
+
+  collection.options = {
+    "query": "SELECT \n  p1.id,\n  p1.description,\n  p1.created,\n  p1.updated,\n  p1.payment_date AS dated,\n  p1.invoice,\n  p1.party,\n  p1.type,\n  p1.paid,\n  (CASE WHEN p1.type = 'recieving' THEN \n      (CASE WHEN p1.paid = TRUE THEN p1.amount\n       ELSE 0 \n       END) \n   ELSE (CASE WHEN p1.type = 'sending' THEN \n           (CASE WHEN p1.paid = FALSE THEN p1.amount\n            ELSE 0 \n            END) \n         ELSE 0 -- return case missing\n         END)\n   END) AS credit,\n  (CASE WHEN p1.type = 'sending' THEN \n      (CASE WHEN p1.paid = TRUE THEN p1.amount\n       ELSE 0 \n       END) \n   ELSE (CASE WHEN p1.type = 'recieving' THEN \n           (CASE WHEN p1.paid = FALSE THEN p1.amount\n            ELSE 0 \n            END) \n         ELSE 0 -- return case missing\n         END)\n   END) AS debit\nFROM payments_view p1 ORDER BY p1.payment_date\n"
+  }
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "x1kfc8ab",
+    "name": "description",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "gkyyje5k",
+    "name": "dated",
+    "type": "json",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 1
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "12dpfahq",
+    "name": "invoice",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "0pwj3vph7ag7nc2",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "85rztpen",
+    "name": "party",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "qhdujcbptsh2v29",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "jw0gbypl",
+    "name": "type",
+    "type": "select",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "values": [
+        "sending",
+        "recieving",
+        "return"
+      ]
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "vfao2udd",
+    "name": "paid",
+    "type": "bool",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "dpkto1ef",
+    "name": "credit",
+    "type": "json",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 1
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "chj5hhku",
+    "name": "debit",
+    "type": "json",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 1
+    }
+  }))
+
+  // remove
+  collection.schema.removeField("lehnhtov")
+
+  // remove
+  collection.schema.removeField("ddr5l4yz")
+
+  // remove
+  collection.schema.removeField("menar0a5")
+
+  // remove
+  collection.schema.removeField("kda64c2c")
+
+  // remove
+  collection.schema.removeField("heqmbep8")
+
+  // remove
+  collection.schema.removeField("fn7simnp")
+
+  // remove
+  collection.schema.removeField("8ev2sk5b")
+
+  // remove
+  collection.schema.removeField("dgmmqzpf")
+
+  return dao.saveCollection(collection)
+})
--- frontend/src/app/app/reports/ledgers/booker/page.tsx
+++ frontend/src/app/app/reports/ledgers/booker/page.tsx
@@ -1,59 +1,55 @@
 "use client";
-
 import StatusCheck, { checkSuccess } from "@/app/api/StatusCheck";
 import useCRUD from "@/app/api/useAPI";
-import DataViewTable from "@/app/components/DataViewTable";
+import { NSelect } from "@/app/components/BetterComps/Select";
 import PrintContent from "@/app/components/printing/PrintContent";
-import PrintHead from "@/app/components/printing/PrintHead";
-import { ActionIcon, Flex, Group, NumberInput, Stack, Table, Text } from "@mantine/core";
+import { ActionIcon, Button, Flex, Group, NumberInput, Stack, Table, Text } from "@mantine/core";
 import { DateInput } from "@mantine/dates";
 import { IconPrinter } from "@tabler/icons-react";
 import { DataTableColumn } from "mantine-datatable";
 import moment from "moment";
+import Link from "next/link";
 import { useRef, useState } from "react";
 import { useReactToPrint } from "react-to-print";
 
 const tableStructure: DataTableColumn[] = [
   { accessor: "id", hidden: true },
-  { accessor: "created", width: "7em", title: "Date", render: (row) => String(row.created).slice(0, 10) },
-  { accessor: "invoiceNo", title: "Inv #", width: "5em" },
-  { accessor: "description" },
-  { accessor: "transaction_type", title: "Type" },
-  { accessor: "account_type", title: "Account" },
+  { accessor: "dated", width: "7em", title: "Date", render: (row) => String(row.created).slice(0, 10) },
   {
-    accessor: "accounts_recievable",
-    title: "Recievables",
+    accessor: "invoice",
+    title: "Inv #",
+    width: "5em",
+    render: (row: any) => (
+      <Link style={{ textDecoration: "none" }} href={`/app/invoices/print?invoiceId=${row.invoice}`}>
+        {row.expand?.invoice.invoiceNo}
+      </Link>
+    ),
+  },
+  { accessor: "entry", render: (row: any) => (row.description === "payment" ? "Payment" : "Invoice") },
+  {
+    accessor: "credit",
     textAlign: "right",
-    render: (record: any) =>
-      !isNaN(record.accounts_recievable)
-        ? Number(record.accounts_recievable).toFixed(2)
-        : record.accounts_recievable,
+    render: (record: any) => (!isNaN(record.credit) ? Number(record.credit).toFixed(2) : record.credit),
   },
   {
-    accessor: "accounts_payable",
-    title: "Payables",
+    accessor: "debit",
     textAlign: "right",
-    render: (record: any) =>
-      !isNaN(record.accounts_payable) ? Number(record.accounts_payable).toFixed(2) : record.accounts_payable,
-  },
-  {
-    accessor: "cash",
-    textAlign: "right",
-    render: (record: any) => (!isNaN(record.cash) ? Number(record.cash).toFixed(2) : record.cash),
+    render: (record: any) => (!isNaN(record.debit) ? Number(record.debit).toFixed(2) : record.debit),
   },
 ];
-
-export default function LedgerReport() {
+export default function BookerLedgerReport() {
   const [fromDate, setFromDate] = useState<Date | null>(moment().startOf("month").toDate());
   const [toDate, setToDate] = useState<Date | null>(moment().endOf("day").toDate());
+  const [booker, setBooker] = useState<any>("");
   const ledger = useCRUD().fullList({
-    collection: "ledger_journal",
+    collection: "ledger_booker",
+    expand: "invoice,party,invoice.booker",
     sort: "+created",
     filter: `(created >= '${moment(fromDate)
       .startOf("day")
       .format("YYYY-MM-DD HH:mm:ss")}' && created <= '${moment(toDate)
       .endOf("day")
-      .format("YYYY-MM-DD HH:mm:ss")}')`,
+      .format("YYYY-MM-DD HH:mm:ss")}' ) ${booker ? `&& invoice.booker = '${booker}'` : ""}`,
   });
   const printRef = useRef();
   const handlePrint = useReactToPrint({
@@ -61,44 +57,12 @@ export default function LedgerReport() {
     pageStyle: "@page {size: A4; margin: 0.6cm 1.0cm 0.6cm 0.5cm !important;",
   });
 
-  const [openingRecievables, setOpeningRecievables] = useState(0);
-  const [openingPayables, setOpeningPayables] = useState(0);
-  const [openingCash, setOpeningCash] = useState(0);
-  const [openingStock, setOpeningStock] = useState(0);
-
-  const opening = {
-    id: "",
-    collectionId: "",
-    description: "Opening Balance",
-    collectionName: "",
-    created: "",
-    transaction_type: "",
-    account_type: "",
-    accounts_recievable: openingRecievables,
-    cash: openingCash,
-    accounts_payable: openingPayables,
-    stock: openingStock,
-  } as any;
-  const { recievable, payable, cash, stock } = calculator(
-    ledger.data ? [opening, ...ledger.data] : [opening]
-  );
-  const totals = {
-    account_type: <Text fw={700}>Totals</Text>,
-    collectionId: "",
-    collectionName: "",
-    created: "",
-    description: "",
-    id: "",
-    accounts_recievable: <Text fw={700}>{Number(recievable)}</Text>,
-    accounts_payable: <Text fw={700}>{Number(payable)}</Text>,
-    cash: <Text fw={700}>{Number(cash)}</Text>,
-    stock: Number(stock),
-    transaction_type: "",
-    textAlign: "right",
-  };
   const queries = [ledger];
   if (checkSuccess(queries)) {
-    const allData = [opening, ...ledger.data, totals];
+    const allData = [...ledger.data];
+    const credit = allData.reduce((total, record) => total + record.credit, 0);
+    const debit = allData.reduce((total, record) => total + record.debit, 0);
+    const total = credit - debit;
     return (
       <>
         <Stack mb={"sm"}>
@@ -112,7 +76,7 @@ export default function LedgerReport() {
           >
             <IconPrinter />
           </ActionIcon>
-          <Group>
+          <Group align='end'>
             <DateInput
               value={fromDate}
               onChange={(v) => setFromDate(moment(v).startOf("day").utc().toDate())}
@@ -123,110 +87,101 @@ export default function LedgerReport() {
               onChange={(v) => setToDate(moment(v).endOf("day").utc().toDate())}
               label='Date To'
             />
-          </Group>
-          <Group>
-            <NumberInput
-              label='Opening Recievables'
-              value={openingRecievables}
-              onChange={(v) => setOpeningRecievables(Number(v))}
+            <NSelect
+              searchable
+              value={booker}
+              onChange={setBooker}
+              label='Booker'
+              dataQuery={{
+                collectionName: "order_bookers",
+              }}
+              dataQueryValue={"id"}
+              dataLabel={"name"}
             />
-            <NumberInput
-              label='Opening Payables'
-              value={openingPayables}
-              onChange={(v) => setOpeningPayables(Number(v))}
-            />
-            <NumberInput
-              label='Opening Cash'
-              value={openingCash}
-              onChange={(v) => setOpeningCash(Number(v))}
-            />
-            {/* <NumberInput
-              label='Opening Stock'
-              value={openingStock}
-              onChange={(v) => setOpeningStock(Number(v))}
-            /> */}
+            <Button onClick={() => setBooker("")}>Clear</Button>
           </Group>
           <div style={{ marginLeft: "1em", marginRight: "1em" }} ref={printRef}>
-            <PrintContent>
-              <Text size='xl' fw={700}>
-                {" "}
-                GENERAL LEDGER{" "}
-              </Text>
-              <Table
-                fz={"10pt"}
-                horizontalSpacing={1}
-                verticalSpacing={0}
-                styles={{
-                  td: { fontSize: "md", padding: "0.2em", border: "1px solid black" },
-                  th: { fontSize: "md", padding: "0.2em", border: "1px solid black" },
-                }}
-              >
-                <Table.Thead>
-                  <Table.Tr>
-                    {tableStructure.map((col) =>
-                      col.hidden ? null : (
-                        <Table.Td key={`thead-${col.accessor}`}>
-                          {String(col.title ?? col.accessor).toUpperCase()}
-                        </Table.Td>
-                      )
-                    )}
-                  </Table.Tr>
-                </Table.Thead>
-                <Table.Tbody>
-                  {allData.map((row) => (
-                    <Table.Tr
-                      key={`row-${row.id}`}
-                      style={
-                        (row.transaction_type === "payment" || row.transaction_type === "expense")
-                          ? row.account_type === "recieving"
-                            ? { color: "green" }
-                            : { color: "red"}
-                          : row.account_type === "sending" ? { color:"blue"} : {color:"black"}
-                      }
-                    >
-                      {tableStructure.map((col, i) =>
+            {!booker ? (
+              <Text size='xl'>PLEASE SELECT A BOOKER TO VIEW LEDGER</Text>
+            ) : (
+              <PrintContent>
+                <Text size='xl' fw={700}>
+                  {" "}
+                  BOOKER SALES LEDGER{" "}
+                </Text>
+                <Text>Booker: {allData[0]?.expand?.invoice?.expand.booker?.name ?? "NO DATA FOUND FOR SELECTED PARTY"}</Text>
+                <Table
+                  stickyHeader
+                  stickyHeaderOffset={50}
+                  fz={"10pt"}
+                  horizontalSpacing={1}
+                  verticalSpacing={0}
+                  styles={{
+                    td: { fontSize: "md", padding: "0.2em", border: "1px solid black" },
+                    th: { fontSize: "md", padding: "0.2em", border: "1px solid black" },
+                  }}
+                >
+                  <Table.Thead>
+                    <Table.Tr>
+                      {tableStructure.map((col) =>
                         col.hidden ? null : (
-                          <Table.Td
-                            key={`td-${row.id}-${col.accessor}`}
-                            style={{ textAlign: col.textAlign ?? "start", width: col.width ?? "auto" }}
-                          >
-                            {col.render
-                              ? col.render(row as any, i)
-                              : row.hasOwnProperty("expand")
-                              ? row.expand.hasOwnProperty(col.accessor)
-                                ? row.expand[col.accessor].name || row.expand[col.accessor].value
-                                : row[col.accessor]
-                              : row[col.accessor]}
+                          <Table.Td key={`thead-${col.accessor}`}>
+                            {String(col.title ?? col.accessor).toUpperCase()}
                           </Table.Td>
                         )
                       )}
                     </Table.Tr>
-                  ))}
-                </Table.Tbody>
-              </Table>
-            </PrintContent>
+                  </Table.Thead>
+                  <Table.Tbody>
+                    {allData.map((row) => (
+                      <Table.Tr key={`row-${row.id}`}>
+                        {tableStructure.map((col, i) =>
+                          col.hidden ? null : (
+                            <Table.Td
+                              key={`td-${row.id}-${col.accessor}`}
+                              style={{ textAlign: col.textAlign ?? "start", width: col.width ?? "auto" }}
+                            >
+                              {col.render
+                                ? col.render(row as any, i)
+                                : row.hasOwnProperty("expand")
+                                ? row.expand.hasOwnProperty(col.accessor)
+                                  ? row.expand[col.accessor].name || row.expand[col.accessor].value
+                                  : row[col.accessor]
+                                : row[col.accessor]}
+                            </Table.Td>
+                          )
+                        )}
+                      </Table.Tr>
+                    ))}
+                  </Table.Tbody>
+                </Table>
+                <Flex justify={"end"}>
+                  <Stack gap={0} w={"30%"}>
+                    <Group justify='space-between'>
+                      <Text fw={600}>Total Credit:</Text>
+                      <Text my={0} fw={600} style={{ textAlign: "end" }}>
+                        {credit.toFixed(2)}
+                      </Text>
+                    </Group>
+                    <Group justify='space-between'>
+                      <Text fw={600}>Total Debit:</Text>
+                      <Text my={0} fw={600} style={{ textAlign: "end" }}>
+                        {debit.toFixed(2)}
+                      </Text>
+                    </Group>
+                    <Group justify='space-between'>
+                      <Text fw={600}>Final Balance:</Text>
+                      <Text my={0} fw={600} style={{ textAlign: "end" }}>
+                        {total.toFixed(2)}
+                      </Text>
+                    </Group>
+                  </Stack>
+                </Flex>
+              </PrintContent>
+            )}
           </div>
         </Stack>
       </>
     );
   } else <StatusCheck check={queries} />;
-}
-
-function calculator(data) {
-  let accounts_recievable = 0;
-  let accounts_payable = 0;
-  let cash = 0;
-  let stock = 0;
-  data?.map((row) => {
-    accounts_recievable += Number(row.accounts_recievable);
-    accounts_payable += Number(row.accounts_payable);
-    cash += Number(row.cash);
-    stock += Number(row.stock);
-  });
-  return {
-    recievable: Number(accounts_recievable).toFixed(2),
-    payable: Number(accounts_payable).toFixed(2),
-    cash: Number(cash).toFixed(2),
-    stock: Number(stock).toFixed(2),
-  };
 }
