4e42cbc9700287d753897d5d4aec47a43fb95bbf
Abdul Hakam <zehas.11@gmail.com>
2026-04-15 09:54:44 +0500

    enabled trailbase api for tables

---
+++ trailbase/traildepot/migrations/main/U1776174441__create_table_companies.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "companies" (
+  'id' BLOB PRIMARY KEY CHECK (is_uuid_v7(id)) NOT NULL,
+  'name' TEXT NOT NULL,
+  'created' TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP CHECK (datetime(created) IS NOT NULL),
+  'updated' TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP CHECK (datetime(updated) IS NOT NULL)
+) STRICT;
--- trailbase/traildepot/migrations/main/U1776174445__create_table_products.sql
+++ trailbase/traildepot/migrations/main/U1776174445__create_table_products.sql
@@ -1,6 +1,7 @@
 CREATE TABLE "products" (
     'id' BLOB PRIMARY KEY CHECK (is_uuid_v7(id)) NOT NULL,
     'category' BLOB NOT NULL REFERENCES 'categories'('id'),
+    'company' BLOB NOT NULL REFERENCES 'companies'('id'),
     'cost_price' REAL NOT NULL DEFAULT 0,
     'name' TEXT NOT NULL DEFAULT '',
     'sale_price' REAL NOT NULL DEFAULT 0,
