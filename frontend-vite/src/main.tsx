3a1d65af20455ab24aeef4fcdb07f5bfe0f35ff8
Abdul Hakam <zehas.11@gmail.com>
2026-04-15 13:19:20 +0500

    completed adding the tanstack router.

--- frontend-vite/src/main.tsx
+++ frontend-vite/src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import App from "./App.tsx";
 
+const router = createRouter({ routeTree });
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider defaultColorScheme='auto'>
+      <RouterProvider router={router} />
       <App />
     </MantineProvider>
   </StrictMode>,
