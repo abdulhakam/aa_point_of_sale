d67e2985d1ab26f22e8dd20b760f31437d9d9e7e
Abdul Hakam <zehas.11@gmail.com>
2026-04-15 12:45:21 +0500

    added tanstack router and db, along with trailbase db collection

--- frontend-vite/eslint.config.js
+++ frontend-vite/eslint.config.js
@@ -5,10 +5,10 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default defineConfig( [
+  globalIgnores( [ 'dist' ] ),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: [ '**/*.{ts,tsx}' ],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -20,4 +20,4 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+] )
