219f62547a60e32aca53cb641e3c6ea0a0336658
Abdul Hakam <zehas.11@gmail.com>
2026-04-18 13:59:22 +0500

    automatic generation of routes in management pages

--- frontend-vite/src/routes/app/management/index.tsx
+++ frontend-vite/src/routes/app/management/index.tsx
@@ -1,18 +1,23 @@
 import { Anchor, Stack } from "@mantine/core";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/app/management/")({
   component: RouteComponent,
 });
 
-const links = ["sections"];
-
 function RouteComponent() {
+  const router = useRouter();
+  const thisRoute = "app/management";
+
+  // Find the management route and get its children
+  const managementRouteLinks = router.routeTree.children
+    .filter((route) => route.path.includes(thisRoute))
+    .slice(1);
   return (
     <Stack>
-      {links.map((link) => (
-        <Anchor key={link} href={`/app/management/${link}`}>
-          {link}
+      {managementRouteLinks.map((link) => (
+        <Anchor key={link.path} href={`/${link.path}`}>
+          {link.path.split("/").slice(2)[0]}
         </Anchor>
       ))}
     </Stack>
