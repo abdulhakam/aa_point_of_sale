0b2db724c25186139dabe93246ea209bfeb83113
Abdul Hakam <zehas.11@gmail.com>
2026-04-29 14:29:37 +0500

    initial router config

--- frontend-tb/vite.config.js
+++ frontend-tb/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig( {
+  plugins: [ tanstackRouter( {
+    target: 'react',
+    autoCodeSplitting: true,
+  } ), react() ],
+} )
