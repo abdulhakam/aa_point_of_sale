8f578b7dfc8990d73c8354eaf28308e5ab1dbdea
Abdul Hakam <zehas.11@gmail.com>
2026-05-06 17:14:48 +0500

    feat(associations): create companies2orderbookers join table
    Adds a join table to establish many-to-many relationship between
    companies and order_bookers, enabling proper association
    synchronization. Also updates generated route tree to reflect new
    API endpoints.

--- trailbase/traildepot/migrations/main/U1776174443__create_table_order_bookers.sql
+++ trailbase/traildepot/migrations/main/U1776174443__create_table_order_bookers.sql
@@ -1,6 +1,5 @@
 CREATE TABLE "order_bookers" (
-    'id' BLOB PRIMARY KEY CHECK (is_uuid_v7(id)) DEFAULT (uuid_v7()) NOT NULL,
-    'company' TEXT NOT NULL DEFAULT '{}',
+    'id' BLOB PRIMARY KEY CHECK (is_uuid_v7 (id)) DEFAULT (uuid_v7 ()) NOT NULL,
     'name' TEXT NOT NULL DEFAULT '',
     'phone' TEXT NOT NULL DEFAULT '',
     'created' INTEGER NOT NULL DEFAULT (CURRENT_TIMESTAMP),
